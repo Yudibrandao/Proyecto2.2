14fe856e1b5af2ffd606b71615758d52fc019bba
´Yudith´ <yudi@geeksacademy.es>
2023-10-10 18:12:13 +0200

    Proyecto2-2

---
+++ README.md
@@ -0,0 +1,41 @@
+#
+
+#### Table of Contents
+
+- [How to run](#How-to-run-)
+- [DB](#DB-)
+- [Backend](#Backend-)
+    -[User endpoints](#USER)
+    -[Product endpoints](#MOVIE)
+
+-  [Frontend](#Fronted-)
+
+#
+
+## CONSOLA DE NINTENDO SWITCH -[Frontend](#Frontend-)
+
+Les presento mi segundo proyecto, una Consola de Nintendo Switch con un comando lateral de cada lado, tanto del lado derecho como del lado izquierdo.
+
+Consta de cuatro botones interactivos:
+
+
+![Consola](image-1.png)
+
+Del lado izquierdo encontramos un boton inferior que nos permite encender y encender y apagar nuestra consolas, al hacer clic en el boton de la parte superior podemos jugar un videojuego de MarioBross
+
+Del lado derecho encontramos un boton inferior que nos llevara a una actualizacion de dicha consola y por ultimo pero no menos importante en la parte superior encontramos un GifDivertido de MarioBross.
+
+
+## AUTOR 
+
+    Yudith Brandao
+
+## INFORMACION DE CONTACTO
+
+    Telefono Mobil: +58 000 000
+    Gmail yudi@brandao.com
+
+
+## SECCION DE BUGS CONOCIDOS
+
+    En ocasiones el juego aparece mas grande de lo debido... Se encuentra en reparacion...
---
+++ src/js/jmain.js
@@ -0,0 +1,63 @@
+
+
+const encender=()=> {
+
+    let p = document.getElementById("pantalla");
+    if (p.classList.contains("on")) {
+        p.innerHTML = "";
+        p.classList.remove("on");
+    } else {
+
+        p.innerHTML = "<img src='src/img/logo.webp' class='img-fluid'>";
+        p.classList.add("on");
+    }
+}
+
+const jugar=()=> {
+    let p = document.getElementById("pantalla");
+    if (p.classList.contains("on")) {
+
+        if (!p.classList.contains("video")) {
+
+
+
+            p.innerHTML = '<video id="video"><source src="src/img/mario-Brosss.mp4" type="video/mp4"> Your browser does not support the video tag.</video> ';
+            let video = document.getElementById("video");
+            video.play();
+            p.classList.add("video");
+        }
+    }
+}
+
+const animacion=()=> {
+    let p = document.getElementById("pantalla")
+    if (p.classList.contains("on")) {
+        p.innerHTML = "";
+        p.classList.remove("on");
+
+        if (!p.classList.contains("animacion")) {
+
+
+
+            p.innerHTML = '<img src="src/img/giphy.gif" alt="gif">';
+            let imagen = document.getElementById("imagen");
+            Imagen.gif();
+            p.classList.add("imagen");
+        }
+
+    }
+
+}
+
+const actualizando=()=> {
+    let p = document.getElementById("pantalla");
+    if (p.classList.contains("on")) {
+
+    } else {
+
+        p.innerHTML = "<img src='src/img/cargando.webp' class='img-fluid'>";
+        p.classList.add("on");
+    }
+
+
+}
